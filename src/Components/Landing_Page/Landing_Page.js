ba38f065feaedce60bdd144e16e9789b0efdec3e
Stergianni <stella.orfanidis@hotmail.com>
2025-02-22 23:21:15 +0100

    Exercise 1: Create a React component

---
+++ src/Components/Landing_Page/Landing_Page.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Landing_Page.css";
+
+const Landing_Page = () => {
+  return (
+    <div className="landing">
+      <h1>Welcome to StayHealthy!</h1>
+      <p>Your health, our priority.</p>
+    </div>
+  );
+};
+
+export default Landing_Page;
