3baccde1dfb5498c94a9aff75c88779bb1ad94d9
Stergianni <stella.orfanidis@hotmail.com>
2025-02-22 22:45:49 +0100

    Exercise 3: Set up database connectivity

--- server/db.js
+++ server/db.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
+//const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
+const mongoURI = "mongodb://root:ye9AFBnG97G0Ercgt65DiPrY@172.21.212.21:27017/";
+
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
